9a98059dfbf362c2fb208122ba87c2a75601ac36
wf001 <wf001@diax.xyz>
2024-11-03 19:20:24 +0900

    add test script

---
+++ script/test.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+dir="generated/test/$(date +%s)"
+msg="OK"
+code=0
+total_tests=0
+passed_count=0
+failed_count=0
+
+assert() {
+  expected="$1"
+  input="$2"
+
+  ./"$dir"/out
+
+  actual="$?"
+
+  ((total_tests++))
+
+  if [ "$actual" = "$expected" ]; then
+    ((passed_count++))
+    echo -e "\033[0;32m$input => $actual\033[0m"
+  else
+    ((failed_count++))
+    echo -e "\033[0;31m$input => $expected expected, but got $actual\033[0m"
+    msg="NG"
+    code=-1
+  fi
+}
+
+build(){
+  mkdir -p "$dir"
+  go run main.go > "$dir"/out.ll
+  llc "$dir"/out.ll -o "$dir"/out.s
+  clang "$dir"/out.s -o "$dir"/out
+}
+
+summary(){
+  echo -e "\n------------------------"
+  echo -e "summary: $msg, total: $total_tests, passed: $passed_count, failed: $failed_count"
+  echo -e "------------------------"
+}
+
+testit(){
+  assert 5 '5'
+}
+
+build
+testit
+summary
+
+exit $code
+
