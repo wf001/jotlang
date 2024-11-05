4ed84831ee0b9225cb094cef775aabff3c30d534
wf001 <wf001@diax.xyz>
2024-11-05 11:11:59 +0900

    works to support passing command line args

--- go.mod
+++ go.mod
@@ -2,11 +2,16 @@ module github.com/wf001/jotlang
 
 go 1.23.2
 
-require github.com/llir/llvm v0.3.6
+require (
+	github.com/alecthomas/kingpin/v2 v2.4.0
+	github.com/llir/llvm v0.3.6
+)
 
 require (
+	github.com/alecthomas/units v0.0.0-20211218093645-b94a6e3cc137 // indirect
 	github.com/mewmew/float v0.0.0-20201204173432-505706aa38fa // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/xhit/go-str2duration/v2 v2.1.0 // indirect
 	golang.org/x/mod v0.4.2 // indirect
 	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
 	golang.org/x/tools v0.1.4 // indirect
--- main.go
+++ main.go
@@ -1,13 +1,33 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
+	"os"
+
+	"github.com/alecthomas/kingpin/v2"
 	"github.com/llir/llvm/ir"
 	"github.com/llir/llvm/ir/constant"
 	"github.com/llir/llvm/ir/types"
 )
 
-func main() {
+const (
+	VERSION = "0.0.1"
+	AUTHOR  = "wf001"
+)
+
+var (
+	app = kingpin.New("jotc", "Compiler for the Jot programming language.").Version(VERSION).Author(AUTHOR)
+
+	//logLevel = app.Flag("loglevel", "Set the level of logging to show").Default("info").Enum("debug", "verbose", "info", "warning", "error")
+	verbose  = app.Flag("verbose", "Which log tags to show").Short('v').Default("false").Bool()
+
+	buildCom    = app.Command("build", "Build an executable.")
+	buildOutput = buildCom.Flag("output", "Output binary name.").Short('o').Default("out").String()
+
+	runCom    = app.Command("run", "Build and run an executable.")
+)
+
+func doMain() {
 	m := ir.NewModule()
 
 	globalG := m.NewGlobalDef("g", constant.NewInt(types.I32, 4))
@@ -22,9 +42,18 @@ func main() {
 	funcMain := m.NewFunc(
 		"main",
 		types.I32,
-	)  // omit parameters
+	) // omit parameters
 	mb := funcMain.NewBlock("") // llir/llvm would give correct default name for block without name
 	mb.NewRet(mb.NewCall(funcAdd, constant.NewInt(types.I32, 1), mb.NewLoad(types.I32, globalG)))
 
 	fmt.Println(m)
+
+}
+
+func main() {
+  switch kingpin.MustParse(app.Parse(os.Args[1:])) {
+  // Register user
+  case buildCom.FullCommand():
+    doMain()
+  }
 }
