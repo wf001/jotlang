4c58cb663d47cf58455a3e9d1c3eb6b34ac6f989
wf001 <wf001@diax.xyz>
2024-12-19 12:26:30 +0900

    add matchedOperator test

--- pkg/parser/parser_test.go
+++ pkg/parser/parser_test.go
@@ -1,1 +1,22 @@
 package parser
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	mTypes "github.com/wf001/modo/pkg/types"
+)
+
+func TestNewTokenMap(t *testing.T) {
+	res, ok := matchedOperator(
+		&mTypes.Token{Kind: mTypes.TK_OPERATOR, Val: mTypes.NARY_OPERATOR_ADD},
+	)
+	assert.EqualValues(t, true, ok)
+	assert.EqualValues(t, mTypes.ND_ADD, res)
+
+	res, ok = matchedOperator(
+		&mTypes.Token{Kind: mTypes.TK_BIND, Val: mTypes.SYMBOL_LET},
+	)
+	assert.EqualValues(t, false, ok)
+	assert.EqualValues(t, mTypes.ND_NIL, res)
+}
