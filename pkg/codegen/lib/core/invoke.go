df85d176eceba018de1119530a747760fbf0b77a
wf001 <wf001@diax.xyz>
2025-01-30 20:19:35 +0900

    move invoke method to invoke.go from fold.go

--- pkg/codegen/lib/core/invoke.go
+++ pkg/codegen/lib/core/invoke.go
@@ -2,6 +2,7 @@ package core
 
 import (
 	"github.com/llir/llvm/ir"
+	"github.com/llir/llvm/ir/enum"
 	"github.com/llir/llvm/ir/value"
 
 	mTypes "github.com/wf001/modo/pkg/types"
@@ -16,4 +17,34 @@ var LibInsts = map[string]func(*ir.Block, *mTypes.BuiltinLibProp, *mTypes.Node) 
 	mTypes.OPERATOR_EQ:  InvokeEq,
 	// binary
 	mTypes.OPERATOR_MOD: InvokeMod,
+}
+
+func InvokeAdd(block *ir.Block, libs *mTypes.BuiltinLibProp, node *mTypes.Node) value.Value {
+	return invokeFold(node, func(x, y value.Value) value.Value {
+		return block.NewAdd(x, y)
+	})
+}
+
+func InvokeSub(block *ir.Block, libs *mTypes.BuiltinLibProp, node *mTypes.Node) value.Value {
+	return invokeFold(node, func(x, y value.Value) value.Value {
+		return block.NewSub(x, y)
+	})
+}
+
+func InvokeMul(block *ir.Block, libs *mTypes.BuiltinLibProp, node *mTypes.Node) value.Value {
+	return invokeFold(node, func(x, y value.Value) value.Value {
+		return block.NewMul(x, y)
+	})
+}
+
+func InvokeMod(block *ir.Block, libs *mTypes.BuiltinLibProp, node *mTypes.Node) value.Value {
+	return invokeFold(node, func(x, y value.Value) value.Value {
+		return block.NewSRem(x, y)
+	})
+}
+
+func InvokeEq(block *ir.Block, libs *mTypes.BuiltinLibProp, node *mTypes.Node) value.Value {
+	return invokeFold(node, func(x, y value.Value) value.Value {
+		return block.NewICmp(enum.IPredEQ, x, y)
+	})
 }
