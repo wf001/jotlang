24510cea50474d9d5a4aa15f83d5a8721cf16bc3
wf001 <wf001@diax.xyz>
2025-02-04 23:38:23 +0900

    remove dead code

--- pkg/codegen/lib/core/prn.go
+++ pkg/codegen/lib/core/prn.go
@@ -16,29 +16,19 @@ func InvokePrn(block *ir.Block, libs *mTypes.BuiltinLibProp, node *mTypes.Node) 
 		value := n.IRValue
 		ty := n.IRValue.Type()
 
-		if n.IsType(mTypes.TY_INT32) || ty.Equal(types.I32) {
+		if ty.Equal(types.I32) {
 			formatStr = libs.GlobalVar.FormatDigit
 
-		} else if n.IsType(mTypes.TY_BOOL) || ty.Equal(types.I1) {
+		} else if ty.Equal(types.I1) {
 			formatStr = libs.GlobalVar.FormatStr
 			value = block.NewSelect(n.IRValue, libs.GlobalVar.TrueValue, libs.GlobalVar.FalseValue)
 
-		} else if n.IsType(mTypes.TY_STR) || ty.Equal(types.I8Ptr) {
+		} else if ty.Equal(types.I8Ptr) {
 			formatStr = libs.GlobalVar.FormatStr
 
-		} else if n.IsKind(mTypes.ND_FUNCCALL) {
-			if ty.Equal(types.I32) {
-				formatStr = libs.GlobalVar.FormatDigit
-			} else if ty.Equal(types.I1) {
-				formatStr = libs.GlobalVar.FormatStr
-				value = block.NewSelect(n.IRValue, libs.GlobalVar.TrueValue, libs.GlobalVar.FalseValue)
-			} else if ty.Equal(types.Void) {
-				formatStr = libs.GlobalVar.FormatStr
-			} else if _, ok := ty.(*types.PointerType); ok {
-				formatStr = libs.GlobalVar.FormatStr
-			} else {
-				log.Panic("unresolved type: have %+v", n)
-			}
+		} else if ty.Equal(types.Void) {
+			formatStr = libs.GlobalVar.FormatStr
+
 		} else {
 			log.Panic("unresolved type: have %+v", n)
 		}
